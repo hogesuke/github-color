eea7a11cf1ffa35dbe36ec4d53ea1a5bcea12ea2
hogesuke <miyado@gmail.com>
2017-02-16 13:05:46 +0900

    元に戻すボタンを追加（仮）

--- chrome-extension/js/script.js
+++ chrome-extension/js/script.js
@@ -1,8 +1,9 @@
 $(function () {
-  var $header   = $('body > .header');
+  var $header   = $('body .header');
   var $button   = $('<a class="dropdown-item header-color-button">Header color</a>');
   var $picker   = $('<div id="colorpicker-container"><div id="colorpicker"></div></div>');
   var $close    = $('<a class="close"></a>');
+  var $default  = $('<a class="default">もとに戻す</a>');
   var $unread   = $('.notification-indicator .mail-status');
   var $pulldown = $('.user-nav > li:last-child');
   var $menu     = $pulldown.find('.dropdown-menu.dropdown-menu-sw');
@@ -32,16 +33,23 @@ $(function () {
     $pulldown.click();
   };
 
-  $('body').append($picker.prepend($close));
+  $('body').append($picker.prepend($close).append($default));
   $menu.append('<div class="dropdown-divider"></div>').append($button);
   $(document).on('click', '.header-color-button', handleButtonClick);
 
-  var initialColor = loadHeaderColor() || $header.css('backgroundColor');
+  var defaultColor = $header.css('backgroundColor');
+  var initialColor = loadHeaderColor() || defaultColor;
+  var picker = $.farbtastic('#colorpicker', colorChangeCallback);
 
-  $.farbtastic('#colorpicker', colorChangeCallback).setColor(initialColor);
+  picker.setColor(initialColor);
   setHeaderColor(initialColor);
 
   $close.on('click', function () {
     $picker.toggle();
+  });
+
+  $default.on('click', function () {
+    // todo rgbを16進数に変換する必要あり
+    picker.setColor(defaultColor);
   });
 });
